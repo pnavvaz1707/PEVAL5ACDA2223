38ccd8b9a52de3512dcaed0ea74245453b9bf4a5
Pnavvaz <120363753+Pnavvaz@users.noreply.github.com>
2023-02-10 08:52:25 +0100

    Casi termiano

--- src/main/java/net/codejava/hibernate/VuelosMain.java
+++ src/main/java/net/codejava/hibernate/VuelosMain.java
@@ -18,7 +18,10 @@ public class VuelosMain {
         vuelosMain.setup();
 
 //        vuelosMain.insertarPasaje();
-        vuelosMain.consultarVuelo();
+        vuelosMain.consultarPasaje();
+//        vuelosMain.actualizarPasajero();
+//        vuelosMain.darAltaPasajesDeUnPasajero();
+        vuelosMain.obtenerImporteRecaudadoDeUnVuelo();
 
         vuelosMain.exit();
     }
@@ -70,7 +73,7 @@ public class VuelosMain {
         session.close();
     }
 
-    private void consultarVuelo() {
+    private void consultarPasaje() {
         Session session = sessionFactory.openSession();
         Scanner teclado = new Scanner(System.in);
 
@@ -87,6 +90,68 @@ public class VuelosMain {
             System.out.println("\tCLASE: " + pasaje.getClase());
             System.out.println("\t\tNombre pasajero: " + pasaje.getPasajero().getNombre() + "\tCódigo pasaje: " + pasaje.getCod() + "\tNúmero de asiento: " + pasaje.getNumAsiento());
         }
+
+        session.close();
+    }
+
+    private void actualizarPasajero() {
+        Session session = sessionFactory.openSession();
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce el código del pasajero que desee modificar");
+        int codPasajeroSel = teclado.nextInt();
+        teclado.nextLine();
+
+        System.out.println("Introduce el nombre nuevo del pasajero");
+        String nombreNuevo = teclado.nextLine();
+
+        System.out.println("Introduce el teléfono nuevo del pasajero");
+        String telNuevo = teclado.nextLine();
+
+        System.out.println("Introduce la dirección nueva del pasajero");
+        String direccionNueva = teclado.nextLine();
+
+        System.out.println("Introduce el país nuevo del pasajero");
+        String paisNuevo = teclado.nextLine();
+
+        Query q = session.createQuery("UPDATE Pasajero SET nombre = :nombreNuevo, telefono = :telNuevo, direccion = :direccionNueva, pais = :paisNuevo  WHERE cod = :codPasajeroSel");
+        q.setParameter("codPasajeroSel", codPasajeroSel);
+        q.setParameter("nombreNuevo", nombreNuevo);
+        q.setParameter("telNuevo", telNuevo);
+        q.setParameter("direccionNueva", direccionNueva);
+        q.setParameter("paisNuevo", paisNuevo);
+
+        System.out.println("Se han modificado " + q.executeUpdate() + " registros");
+
+        session.close();
+    }
+
+    private void darAltaPasajesDeUnPasajero() {
+        Session session = sessionFactory.openSession();
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce el código del pasajero al cual desee darle de alta todos sus pasajes");
+        int codPasajeroSel = teclado.nextInt();
+        teclado.nextLine();
+
+        Query q = session.createQuery("DELETE  FROM Pasaje WHERE pasajero.cod = :codPasajeroSel");
+        q.setParameter("codPasajeroSel", codPasajeroSel);
+
+        System.out.println("Se han borrado " + q.executeUpdate() + " pasajes");
+        session.close();
+    }
+
+    private void obtenerImporteRecaudadoDeUnVuelo() {
+        Session session = sessionFactory.openSession();
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce el identificador del vuelo del cual desea obtener el importe total");
+        String identificadorSel = teclado.nextLine();
+
+        Query q = session.createQuery("SELECT SUM(pvp) FROM Pasaje WHERE vuelo.identificador = :identificadorSel");
+        q.setParameter("identificadorSel", identificadorSel);
+
+        System.out.println("El importe total del vuelo " + identificadorSel + " es de: " + q.uniqueResult() + " €");
 
         session.close();
     }
